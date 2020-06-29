34c26c7aac139ffa03539064e24e1a65229d149e
Khang <khangb612@gmail.com>
2020-06-29 19:59:48 +1000

    update: finished login UI

--- app/src/main/java/com/universal/homear/MainActivity.java
+++ app/src/main/java/com/universal/homear/MainActivity.java
@@ -2,8 +2,10 @@ package com.universal.homear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,5 +21,30 @@ public class MainActivity extends AppCompatActivity {
         mLogin = findViewById(R.id.tv_login);
         mRegister = findViewById(R.id.tv_signup);
         mRegister.setPaintFlags(mRegister.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        mLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchLoginActivity();
+            }
+        });
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchSignUpActivity();
+            }
+        });
     }
+
+    private void launchLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    private void launchSignUpActivity() {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
+    }
+
 }
