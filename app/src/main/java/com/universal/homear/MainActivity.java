a1a051a81443d0152555a81a62b46e8d232c390c
Khang <khangb612@gmail.com>
2020-06-29 18:03:05 +1000

    update: starting authentication activities

--- app/src/main/java/com/universal/homear/MainActivity.java
+++ app/src/main/java/com/universal/homear/MainActivity.java
@@ -2,13 +2,22 @@ package com.universal.homear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mLogin;
+    private TextView mRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mLogin = findViewById(R.id.tv_login);
+        mRegister = findViewById(R.id.tv_signup);
+        mRegister.setPaintFlags(mRegister.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
