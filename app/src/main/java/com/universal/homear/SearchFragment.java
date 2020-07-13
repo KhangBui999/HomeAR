909934a5dbff49c3c0dfc6c4c3b42c99fbaf06af
Khang <khangb612@gmail.com>
2020-07-13 15:28:14 +1000

    chore: AR dependencies added

--- app/src/main/java/com/universal/homear/SearchFragment.java
+++ app/src/main/java/com/universal/homear/SearchFragment.java
@@ -12,6 +12,9 @@ public class SearchFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_search, container, false);
+
+        //Check AR Device Compatibility
+
         return root;
     }
 }
