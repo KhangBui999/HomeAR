2febb3c0ff5575d5f2a3c199fb718e877b1f1a11
Kevin Fan <kevinfan1998@gmail.com>
2020-07-19 18:16:37 +1000

    Added UI elements to activity_register.xml

--- app/src/main/java/com/universal/homear/RegisterActivity.java
+++ app/src/main/java/com/universal/homear/RegisterActivity.java
@@ -2,13 +2,56 @@ package com.universal.homear;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText email, name, password, confirmPassword;
+    private TextView login;
+    private ImageView backbtn;
+    private Button signup;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
+        backbtn = findViewById(R.id.register_backBtn);
+        login = findViewById(R.id.register_login);
+        email = findViewById(R.id.register_email);
+        name = findViewById(R.id.register_name);
+        password = findViewById(R.id.register_ps);
+        confirmPassword = findViewById(R.id.register_confirm);
+        signup = findViewById(R.id.btn_register);
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchLoginActivity();
+            }
+        });
+
+        backbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                launchLoginActivity();
+            }
+        });
+
+
+
+
     }
+
+    private void launchLoginActivity() {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
 }
